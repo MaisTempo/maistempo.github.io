40bc3ef427404bdb6aa7d39395577202a8a3f9b8
Giovanna <ggarciabas@gmail.com>
2020-09-20 19:53:55 -0300

    feat: web site

---
+++ precache-manifest.221de437917ea96c9b53efe5894f9bd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9e41d3d00b68a38d1fcdb2ac6a3fbb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f1c6a6f4e40f549a2e",
+    "url": "/static/js/2.3fa404cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3fa404cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fdb444feb22b2438c1e",
+    "url": "/static/js/main.bb022d51.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "3bd3677427e8040da0d2c9c2cd9a50df",
+    "url": "/static/media/Logo_1.3bd36774.svg"
+  },
+  {
+    "revision": "2c0de1a17a452f10bf66eae8645acd6d",
+    "url": "/static/media/backgroundImg.2c0de1a1.svg"
+  }
+]);
